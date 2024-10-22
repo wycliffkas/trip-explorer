7f1389364fce34dc913c595770d79b52e5c0b0f0
wilbill <ntabazibilly57@gmail.com>
2024-10-21 22:48:42 -0500

    Backend and first commit

--- backend/src/main/java/org/billy/backend/repository/TripRepository.java
+++ backend/src/main/java/org/billy/backend/repository/TripRepository.java
@@ -3,5 +3,5 @@ package org.billy.backend.repository;
 import org.billy.backend.domain.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TripRepository extends JpaRepository<Trip Long> {
+public interface TripRepository extends JpaRepository<Trip, Long> {
 }
